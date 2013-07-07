da057e5eef5dadf28358631f256347d2b5b7b26f
Luis Ibanez <luis.ibanez@kitware.com>
2013-07-07 15:23:36 -0400

    Printing out reference counting with use_count().

--- Source/shared_ptr.cpp
+++ Source/shared_ptr.cpp
@@ -16,6 +16,7 @@ private:
 
 std::shared_ptr<A>  create() {
      std::shared_ptr<A> pp { new A };
+     std::cout << "Create shared count = " << pp.use_count() << std::endl;
      return pp;
 }
 
@@ -23,17 +24,22 @@ std::shared_ptr<A>  create() {
 int main() {
 
     std::shared_ptr<A> a = create();
+     std::cout << "shared count = " << a.use_count() << std::endl;
 
     {   // scope 1
     std::cout << "begin of scope 1" << std::endl;
     std::shared_ptr<A> b = a;
+    std::cout << "shared count = " << b.use_count() << std::endl;
          {  // scope 2
          std::cout << "begin of scope 2" << std::endl;
          std::shared_ptr<A> c = a;
+         std::cout << "shared count = " << c.use_count() << std::endl;
          std::cout << "end of scope 2" << std::endl;
          }
+    std::cout << "shared count = " << b.use_count() << std::endl;
     std::cout << "end of scope 1" << std::endl;
     }
+    std::cout << "shared count = " << a.use_count() << std::endl;
 
     return 0;
 }
